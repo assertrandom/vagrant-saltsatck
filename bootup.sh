056a9ee5f358d19c6ff2f62da063c87052b6030f
Manav Leslie <manav.leslie@gmail.com>
2014-02-26 09:03:47 +0000

    Modified gitignore and added telnet install

--- bootup.sh
+++ bootup.sh
@@ -1,5 +1,6 @@
 sudo mkdir /data
 sudo chmod 777 /data
+sudo yum -y install telnet
 sudo yum -y install java-1.7.0-openjdk-devel
 sudo yum -y install wget
 wget http://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-7/v7.0.52/bin/apache-tomcat-7.0.52.tar.gz -O /data/apache-tomcat-7.0.52.tar.gz
