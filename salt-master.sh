a1f2d5e7cb05dad13e7940a4ae6bd1cfa45b62d9
Manav Leslie <manav.leslie@gmail.com>
2014-03-10 17:06:48 +0000

    Modified salt-master properties

--- salt-master.sh
+++ salt-master.sh
@@ -1,6 +1,6 @@
-#sudo yum -y install salt-master
+sudo yum -y install salt-master
 mkdir /home/vagrant/file_root_base
 chmod 777 /home/vagrant/file_root_base
 cp /home/vagrant/master_shared/master /etc/salt/master
-#sudo chkconfig salt-master on
-#sudo service salt-master start
+sudo chkconfig salt-master on
+sudo service salt-master start
--- salt-minion.sh
+++ salt-minion.sh
@@ -1,4 +1,9 @@
 sudo yum -y install salt-minion
 cp /home/vagrant/minion_shared/minion.txt /etc/salt/minion
-#sudo chkconfig salt-minion on
-#sudo service salt-minion start
+sudo chkconfig salt-minion on
+sudo service salt-minion start
+#wget http://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-7/v7.0.52/bin/apache-tomcat-7.0.52.tar.gz -O /data/apache-tomcat-7.0.52.tar.gz
+#gzip -d /data/apache-tomcat-7.0.52.tar.gz
+#tar -xvf /data/apache-tomcat-7.0.52.tar -C /data
+#/data/apache-tomcat-7.0.52/bin/startup.sh
+
